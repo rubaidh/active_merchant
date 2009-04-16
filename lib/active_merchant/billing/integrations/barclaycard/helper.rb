292a99fc3c80fd760b6f878b374df5e2070ef3eb
Graeme Mathieson <mathie@woss.name>
2009-04-16 17:34:18 +0100

    Mappings for Barclaycard, along with the encryption routine.

--- lib/active_merchant/billing/integrations/barclaycard/helper.rb
+++ lib/active_merchant/billing/integrations/barclaycard/helper.rb
@@ -3,30 +3,84 @@ module ActiveMerchant #:nodoc:
     module Integrations #:nodoc:
       module Barclaycard
         class Helper < ActiveMerchant::Billing::Integrations::Helper
-          # Replace with the real mapping
-          mapping :account, ''
-          mapping :amount, ''
-        
-          mapping :order, ''
+          include PostsData
+
+          def initialize(order, account, options = {})
+            super
+
+            add_field('chargetype', 'Auth')
+            add_field('password', options[:password])
+            add_field('mandatecsc', (options[:mandate_csc] == false) ? "2" : "1")
+
+            add_field('epdqdata', encrypt_transaction_details)
+          end
+
+          # Need to format the amount to have 2 decimal places
+          def amount=(money)
+            cents = money.respond_to?(:cents) ? money.cents : money
+            if money.is_a?(String) or cents.to_i <= 0
+              raise ArgumentError, 'money amount must be either a Money object or a positive integer in cents.'
+            end
+            add_field mappings[:amount], sprintf("%.2f", cents.to_f / 100)
+          end
+
+          mapping :account, 'clientid'
+          mapping :amount, 'total'
+          mapping :currency, 'currencycode'
+
+          mapping :order, 'oid'
 
           mapping :customer, :first_name => '',
                              :last_name  => '',
-                             :email      => '',
-                             :phone      => ''
+                             :email      => 'email',
+                             :phone      => 'btelephonenumber'
 
-          mapping :billing_address, :city     => '',
-                                    :address1 => '',
-                                    :address2 => '',
-                                    :state    => '',
-                                    :zip      => '',
-                                    :country  => ''
+          mapping :billing_address, :city     => 'bcity',
+                                    :address1 => 'baddr1',
+                                    :address2 => 'baddr2',
+                                    :address3 => 'baddr3',
+                                    :state    => 'bcountyprovince',
+                                    :zip      => 'bpostalcode',
+                                    :country  => 'bcountry'
 
-          mapping :notify_url, ''
-          mapping :return_url, ''
+          mapping :shipping_address, :city     => 'scity',
+                                     :address1 => 'saddr1',
+                                     :address2 => 'saddr2',
+                                     :address3 => 'saddr3',
+                                     :state    => 'scountyprovince',
+                                     :zip      => 'spostalcode',
+                                     :country  => 'scountry'
+
+          # Note: Configured in the CPI Admin Tool
+          # mapping :notify_url, ''
+
+          mapping :return_url, 'returnurl'
           mapping :cancel_return_url, ''
           mapping :description, ''
-          mapping :tax, ''
-          mapping :shipping, ''
+          mapping :tax, 'tax'
+          mapping :shipping, 'shipping'
+
+          # Extra special mappings
+          mapping :merchant_display_name, 'merchantdisplayname'
+          mapping :epdq_data, 'epdqdata'
+
+          private
+          def encrypt_transaction_details
+            result = ssl_post(Barclaycard.encryption_url, {
+              :total        => form_fields.delete('total'),
+              :oid          => form_fields.delete('oid'),
+              :chargetype   => form_fields.delete('chargetype'),
+              :password     => form_fields.delete('password'),
+              :currencycode => form_fields.delete('currencycode'),
+              :clientid     => form_fields.delete('clientid'),
+              :mandatecsc   => form_fields.delete('mandatecsc')
+            }.collect { |key, value| "#{key}=#{CGI.escape(value.to_s)}" }.join("&"))
+
+            # result now contains, somewhere, <input name=epdqdata type=hidden value="...">
+            # We want the value of the 'value' attribute and to ignore the rest...
+            # FIXME: Fragile!
+            /value="([^"]+)"/mi.match(result)[1]
+          end
         end
       end
     end
--- test/unit/integrations/helpers/barclaycard_helper_test.rb
+++ test/unit/integrations/helpers/barclaycard_helper_test.rb
@@ -2,42 +2,72 @@ require File.dirname(__FILE__) + '/../../../test_helper'
 
 class BarclaycardHelperTest < Test::Unit::TestCase
   include ActiveMerchant::Billing::Integrations
-  
-  def setup
-    @helper = Barclaycard::Helper.new('order-500','cody@example.com', :amount => 500, :currency => 'USD')
-  end
- 
-  def test_basic_helper_fields
-    assert_field '', 'cody@example.com'
 
-    assert_field '', '5.00'
-    assert_field '', 'order-500'
+  def setup
+    Barclaycard::Helper.any_instance.stubs(:ssl_post).returns("<input value=\"encrypted-data\">")
+
+    @helper = Barclaycard::Helper.new('order-500','1411', :amount => 500, :currency => 'GBP', :password => 'foobar')
   end
-  
+
+  # Fields which are encrypted are removed from the final form.  Despite being
+  # supplied above, they should be nil now.
+  def test_encrypted_fields
+    assert_field 'clientid', nil
+    assert_field 'total', nil
+    assert_field 'oid', nil
+    assert_field 'currencycode', nil
+    assert_field 'mandatecsc', nil
+    assert_field 'password', nil
+    assert_field 'chargetype', nil
+  end
+
+  def test_encryption
+    assert_field 'epdqdata', 'encrypted-data'
+  end
+
   def test_customer_fields
     @helper.customer :first_name => 'Cody', :last_name => 'Fauser', :email => 'cody@example.com'
-    assert_field '', 'Cody'
-    assert_field '', 'Fauser'
-    assert_field '', 'cody@example.com'
+    assert_field 'email', 'cody@example.com'
   end
 
-  def test_address_mapping
+  def test_billing_address_mapping
     @helper.billing_address :address1 => '1 My Street',
                             :address2 => '',
                             :city => 'Leeds',
                             :state => 'Yorkshire',
                             :zip => 'LS2 7EE',
                             :country  => 'CA'
-   
-    assert_field '', '1 My Street'
-    assert_field '', 'Leeds'
-    assert_field '', 'Yorkshire'
-    assert_field '', 'LS2 7EE'
+
+    assert_field 'baddr1', '1 My Street'
+    assert_field 'baddr2', nil
+    assert_field 'bcity', 'Leeds'
+    assert_field 'bcountyprovince', 'Yorkshire'
+    assert_field 'bpostalcode', 'LS2 7EE'
+    assert_field 'bcountry', 'CA'
   end
-  
+
+  def test_shipping_address_mapping
+    @helper.shipping_address :address1 => '1 My Street',
+                             :address2 => '',
+                             :city => 'Leeds',
+                             :state => 'Yorkshire',
+                             :zip => 'LS2 7EE',
+                             :country  => 'CA'
+
+    assert_field 'saddr1', '1 My Street'
+    assert_field 'saddr2', nil
+    assert_field 'scity', 'Leeds'
+    assert_field 'scountyprovince', 'Yorkshire'
+    assert_field 'spostalcode', 'LS2 7EE'
+    assert_field 'scountry', 'CA'
+  end
+
   def test_unknown_address_mapping
+    existing_fields = @helper.fields.size
+
     @helper.billing_address :farm => 'CA'
-    assert_equal 3, @helper.fields.size
+
+    assert_equal existing_fields, @helper.fields.size
   end
 
   def test_unknown_mapping
@@ -45,7 +75,7 @@ class BarclaycardHelperTest < Test::Unit::TestCase
       @helper.company_address :address => '500 Dwemthy Fox Road'
     end
   end
-  
+
   def test_setting_invalid_address_field
     fields = @helper.fields.dup
     @helper.billing_address :street => 'My Street'
