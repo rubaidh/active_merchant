9896f5bfdad06dd1fda7ebb12e1d0a1509d33381
Graeme Mathieson <mathie@woss.name>
2009-04-16 15:43:49 +0100

    Correctly set the service and encryption URLs.

--- lib/active_merchant/billing/integrations/barclaycard.rb
+++ lib/active_merchant/billing/integrations/barclaycard.rb
@@ -5,9 +5,36 @@ module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
     module Integrations #:nodoc:
       module Barclaycard 
-       
-        mattr_accessor :service_url
-        self.service_url = 'https://www.example.com'
+
+        mattr_accessor :test_encryption_url, :production_encryption_url
+        self.test_encryption_url = 'https://secure2.mde.epdq.co.uk/cgi-bin/CcxBarclaysEpdqEncTool.e'
+        self.production_encryption_url = 'https://secure2.epdq.co.uk/cgi-bin/CcxBarclaysEpdqEncTool.e'
+
+        mattr_accessor :test_service_url, :production_service_url
+        self.test_service_url = 'https://secure2.mde.epdq.co.uk/cgi-bin/CcxBarclaysEpdq.e'
+        self.production_service_url = 'https://secure2.epdq.co.uk/cgi-bin/CcxBarclaysEpdq.e'
+
+        def self.encryption_url
+          case ActiveMerchant::Billing::Base.integration_mode
+          when :production
+            self.production_encryption_url
+          when :test
+            self.test_encryption_url
+          else
+            raise StandardError, "Integration mode set to an invalid value: #{ActiveMerchant::Billing::Base.integration_mode}"
+          end
+        end
+
+        def self.service_url
+          case ActiveMerchant::Billing::Base.integration_mode
+          when :production
+            self.production_service_url
+          when :test
+            self.test_service_url
+          else
+            raise StandardError, "Integration mode set to an invalid value: #{ActiveMerchant::Billing::Base.integration_mode}"
+          end
+        end
 
         def self.notification(post)
           Notification.new(post)
--- test/unit/integrations/barclaycard_module_test.rb
+++ test/unit/integrations/barclaycard_module_test.rb
@@ -6,4 +6,21 @@ class BarclaycardModuleTest < Test::Unit::TestCase
   def test_notification_method
     assert_instance_of Barclaycard::Notification, Barclaycard.notification('name=cody')
   end
+
+  def test_test_mode
+    ActiveMerchant::Billing::Base.integration_mode = :test
+    assert_equal 'https://secure2.mde.epdq.co.uk/cgi-bin/CcxBarclaysEpdq.e', Barclaycard.service_url
+    assert_equal 'https://secure2.mde.epdq.co.uk/cgi-bin/CcxBarclaysEpdqEncTool.e', Barclaycard.encryption_url
+  end
+
+  def test_production_mode
+    ActiveMerchant::Billing::Base.integration_mode = :production
+    assert_equal 'https://secure2.epdq.co.uk/cgi-bin/CcxBarclaysEpdq.e', Barclaycard.service_url
+    assert_equal 'https://secure2.epdq.co.uk/cgi-bin/CcxBarclaysEpdqEncTool.e', Barclaycard.encryption_url
+  end
+
+  def test_invalid_mode
+    ActiveMerchant::Billing::Base.integration_mode = :zoomin
+    assert_raise(StandardError){ Barclaycard.service_url }
+  end
 end 
