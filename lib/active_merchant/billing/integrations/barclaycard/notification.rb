8ddd1c2ccce94a3b86c1736de850e09385a33f7a
Mark Connell <mark@rubaidh.com>
2009-04-17 15:36:47 +0100

    Update notifcation.rb for Barclaycard integration

--- lib/active_merchant/billing/integrations/barclaycard/notification.rb
+++ lib/active_merchant/billing/integrations/barclaycard/notification.rb
@@ -6,92 +6,41 @@ module ActiveMerchant #:nodoc:
       module Barclaycard
         class Notification < ActiveMerchant::Billing::Integrations::Notification
           def complete?
-            params['']
+            status == 'Success'
           end 
 
-          def item_id
-            params['']
-          end
-
-          def transaction_id
-            params['']
-          end
-
-          # When was this payment received by the client. 
-          def received_at
-            params['']
-          end
-
-          def payer_email
-            params['']
-          end
-         
-          def receiver_email
-            params['']
-          end 
-
-          def security_key
-            params['']
+          def status
+            params['transactionstatus']
           end
 
           # the money amount we received in X.2 decimal.
           def gross
-            params['']
+            params['total']
           end
 
-          # Was this a test transaction?
-          def test?
-            params[''] == 'test'
+          def account
+            params['clientid']
           end
 
-          def status
-            params['']
+          def transaction_id
+            params['oid']
           end
 
-          # Acknowledge the transaction to Barclaycard. This method has to be called after a new 
-          # apc arrives. Barclaycard will verify that all the information we received are correct and will return a 
-          # ok or a fail. 
-          # 
-          # Example:
-          # 
-          #   def ipn
-          #     notify = BarclaycardNotification.new(request.raw_post)
-          #
-          #     if notify.acknowledge 
-          #       ... process order ... if notify.complete?
-          #     else
-          #       ... log possible hacking attempt ...
-          #     end
-          def acknowledge      
-            payload = raw
-
-            uri = URI.parse(Barclaycard.notification_confirmation_url)
-
-            request = Net::HTTP::Post.new(uri.path)
-
-            request['Content-Length'] = "#{payload.size}"
-            request['User-Agent'] = "Active Merchant -- http://home.leetsoft.com/am"
-            request['Content-Type'] = "application/x-www-form-urlencoded" 
-
-            http = Net::HTTP.new(uri.host, uri.port)
-            http.verify_mode    = OpenSSL::SSL::VERIFY_NONE unless @ssl_strict
-            http.use_ssl        = true
-
-            response = http.request(request, payload)
-
-            # Replace with the appropriate codes
-            raise StandardError.new("Faulty Barclaycard result: #{response.body}") unless ["AUTHORISED", "DECLINED"].include?(response.body)
-            response.body == "AUTHORISED"
+          def charge_type
+            params['chargetype']
           end
- private
 
-          # Take the posted data and move the relevant data into a hash
-          def parse(post)
-            @raw = post
-            for line in post.split('&')
-              key, value = *line.scan( %r{^(\w+)\=(.*)$} ).flatten
-              params[key] = value
-            end
+          # When was this payment received by the client.
+          def received_at
+            Time.strptime(params['datetime'], "%b %d %Y %H:%M:%S")
+          end
+
+          def eci_status
+            params['ecistatus']
+          end
+
+          def card_prefix
+            params['cardprefix']
           end
         end
       end
