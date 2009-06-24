c60af3fb051ab9d78088b68ec966f675771853c1
Mark Connell <mark@rubaidh.com>
2009-06-24 15:22:13 +0100

    Change the way amount is passed to barclaycard

--- lib/active_merchant/billing/integrations/barclaycard/helper.rb
+++ lib/active_merchant/billing/integrations/barclaycard/helper.rb
@@ -21,7 +21,7 @@ module ActiveMerchant #:nodoc:
             if money.is_a?(String) or cents.to_i <= 0
               raise ArgumentError, 'money amount must be either a Money object or a positive integer in cents.'
             end
-            add_field mappings[:amount], sprintf("%.2f", cents.to_f / 100)
+            add_field mappings[:amount], sprintf('%.02f', BigDecimal.new(cents.to_s)/100)
           end
 
           mapping :account, 'clientid'
